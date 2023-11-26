b6bff060dec8a665ba090e533e5ed71300d9ca29
linkmss <59558119+linkmss@users.noreply.github.com>
2023-11-26 19:46:53 +0300

    dev. Template. Delete unused. WIP

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,11 @@
 import { ViteSSG } from 'vite-ssg'
-import { setupLayouts } from 'virtual:generated-layouts'
 
 // import Previewer from 'virtual:vue-component-preview'
 import { routes } from 'vue-router/auto/routes'
 import App from './App.vue'
 import type { UserModule } from './types'
 
-import '@unocss/reset/tailwind.css'
-import './styles/main.css'
-import 'uno.css'
+import '@unocss/reset/tailwind-compat.css'
 
 // const routes = setupLayouts(generatedRoutes)
 
@@ -16,12 +13,12 @@ import 'uno.css'
 export const createApp = ViteSSG(
   App,
   {
-    routes: setupLayouts(routes),
+    routes,
     base: import.meta.env.BASE_URL,
   },
   (ctx) => {
     // install all modules under `modules/`
-    Object.values(import.meta.glob<{ install: UserModule }>('./modules/*.ts', { eager: true }))
+    Object.values(import.meta.glob<{ install: UserModule }>('./plugins/*.ts', { eager: true }))
       .forEach(i => i.install?.(ctx))
     // ctx.app.use(Previewer)
   },
